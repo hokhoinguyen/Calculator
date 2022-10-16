bf61932dc6cd4816a710357e35fd25d6ebbcc7e8
hokhoinguyen <khoinguyen12216@gmail.com>
2022-10-16 14:56:43 +0700

    Xu ly nut Cong

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -46,7 +46,6 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Số A";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
@@ -101,6 +100,7 @@
             this.btCong.TabIndex = 2;
             this.btCong.Text = "+";
             this.btCong.UseVisualStyleBackColor = true;
+            this.btCong.Click += new System.EventHandler(this.btCong_Click_1);
             // 
             // Form1
             // 
--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -17,9 +17,13 @@ namespace Calculator
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void btCong_Click_1(object sender, EventArgs e)
         {
-
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double c = a + b;
+            txtKetQua.Text = c.ToString();
         }
     }
 }
+    
