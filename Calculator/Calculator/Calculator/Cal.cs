9c5133d4b960fe2ce1305ae0674efc9391f56443
hokhoinguyen <khoinguyen12216@gmail.com>
2022-10-23 15:29:36 +0700

    NUnit Bai 1

---
+++ Calculator/Calculator/Calculator/Cal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Cal
+    {
+
+        private int so1 { get; set; }
+        public int so2 { get; set; }
+       
+        public Cal(int x, int y)
+        {
+            so1 = x;
+            so2 = y;
+        }
+        public int Cong()
+        {
+            return so1 + so2;
+        }
+        public int Execute(string z)
+        {
+            int result = 0;
+            switch (z)
+            {
+                case "+":
+                    result = so1 + so2;
+                    break;
+                case "-":
+                    result = so1 - so2;
+                    break;
+                case "*":
+                    result = so1 * this.so2;
+                    break;
+                case "/":
+                    result = so1 / so2;
+                    break;
+                
+            }
+            return result;
+        }
+    }
+}
---
+++ Calculator/Calculator/CalculatorTesting/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace CalculatorTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCong()
+        {
+            Cal cal = new Cal(10 , 5);
+            Assert.AreEqual(15, cal.Execute("+"));
+        }
+
+        [TestMethod]
+        public void TestTru()
+        {
+            Cal cal = new Cal(10, 5);
+            Assert.AreEqual(5, cal.Execute("-"));
+        }
+
+        [TestMethod]
+        public void TestNhan()
+        {
+            Cal cal = new Cal(10, 5);
+            Assert.AreEqual(50, cal.Execute("*"));
+        }
+
+        [TestMethod]
+        public void TestChia()
+        {
+            Cal cal = new Cal(10, 5);
+            Assert.AreEqual(2, cal.Execute("/"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestDivByZero()
+        {
+            Cal cal = new Cal(2, 0);
+            cal.Execute("/");
+        }
+    }
+}
